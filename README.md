4743a58ec6c81c05c1d2208999e22101e8685358
Trust-Coder <mytrustcoder@gmail.com>
2024-08-27 18:20:41 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-images-document
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Trust-Coder/angular-images-document)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    url: 'https://irainbowelectrondeploy.blob.core.windows.net/media/AssessmentMedia/GradeEighth/Mathematics/numbers,operationsandrelationships/exponents/53e40fd4-20240412154322630/53e40fd4-20240412154322630_1.webp',
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    url: 'https://irainbowelectrondeploy.blob.core.windows.net/media/AssessmentMedia/GradeEighth/Mathematics/numbers,operationsandrelationships/exponents/53e40fd4-20240412154322630/53e40fd4-20240412154322630_2.webp',
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    url: 'https://irainbowelectrondeploy.blob.core.windows.net/media/AssessmentMedia/GradeEighth/Mathematics/numbers,operationsandrelationships/exponents/53e40fd4-20240412154322630/53e40fd4-20240412154322630_3.webp',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
